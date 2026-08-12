a1b8c162b2049005d48e66c8878ae15ed3a8b40c
BenoitAud <benoit.audet@arlyx.com>
2026-08-12 14:27:56 -0400

    Tommy chie + easter egg

--- src/GoonerQuestIII.py
+++ src/GoonerQuestIII.py
@@ -14,8 +14,12 @@ SD = pygame.mixer.music.load("For Whom The Bell Tolls (Remastered).mp3")
 PLAYER_WIDTH = 60
 PLAYER_HEIGHT = 120
 PLAYER_VEL = 6
+TRAIL_LIFETIME = 3.0
+TRAIL_RADIUS = 7
+TRAIL_COLOR = (120, 72, 36)
 
-player_image = pygame.transform.scale(pygame.image.load("Tommy.png").convert_alpha(), (PLAYER_WIDTH, PLAYER_HEIGHT))
+tommy_image = pygame.transform.scale(pygame.image.load("Tommy.png").convert_alpha(), (PLAYER_WIDTH, PLAYER_HEIGHT))
+benito_image = pygame.transform.scale(pygame.image.load("benito.png").convert_alpha(), (PLAYER_WIDTH, PLAYER_HEIGHT))
 
 STAR_WIDTH = 20
 STAR_HEIGHT = 90
@@ -26,13 +30,21 @@ big_star = pygame.transform.scale(pygame.image.load("missile2.png").convert_alph
 
 FONT = pygame.font.SysFont("comicsans", 30)
 
-def draw(player, elapsed_time, stars, bstars):
+def draw(player, player_sprite, elapsed_time, stars, bstars, trail_points):
     WIN.blit(BG, (0, 0))
 
     time_text = FONT.render(f"Time: {round(elapsed_time)}s",1 , "red")
     WIN.blit(time_text, (10,10))
 
-    WIN.blit(player_image, (player.x-15, player.y))
+    trail_surface = pygame.Surface((WIDTH, HEIGHT), pygame.SRCALPHA)
+    for x, y, created_at in trail_points:
+        age = elapsed_time - created_at
+        if age < TRAIL_LIFETIME:
+            alpha = int(255 * (1 - (age / TRAIL_LIFETIME)))
+            pygame.draw.circle(trail_surface, (*TRAIL_COLOR, alpha), (x, y), TRAIL_RADIUS)
+    WIN.blit(trail_surface, (0, 0))
+
+    WIN.blit(player_sprite, (player.x-15, player.y))
 
     for star in stars:
         WIN.blit(star_image, star)
@@ -57,6 +69,8 @@ def main():
 
     stars = []
     bstars = []
+    trail_points = []
+    benito_mode = False
     hit = False
 
 
@@ -84,19 +98,26 @@ def main():
                 run = False
                 break
 
+        old_x, old_y = player.x, player.y
+
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LSHIFT]:
             PLAYER_VEL = 2
         else:
             PLAYER_VEL = 6
-        if keys[pygame.K_LEFT] and player.x - PLAYER_VEL >= 0:
-            player.x -= PLAYER_VEL
-        if keys[pygame.K_RIGHT] and player.x + PLAYER_VEL + player.width <= WIDTH:
-            player.x += PLAYER_VEL
-        if keys[pygame.K_UP] and player.y - PLAYER_VEL >= 0:
-            player.y -= PLAYER_VEL
-        if keys[pygame.K_DOWN] and player.y + PLAYER_VEL + player.height <= HEIGHT:
-            player.y += PLAYER_VEL
+        if keys[pygame.K_LEFT]:
+            player.x = max(0, player.x - PLAYER_VEL)
+        if keys[pygame.K_RIGHT]:
+            player.x = min(WIDTH - player.width, player.x + PLAYER_VEL)
+        if keys[pygame.K_UP]:
+            player.y = max(0, player.y - PLAYER_VEL)
+        if keys[pygame.K_DOWN]:
+            player.y = min(HEIGHT - player.height, player.y + PLAYER_VEL)
+
+        if player.x != old_x or player.y != old_y:
+            trail_points.append((player.x + player.width // 2, player.y + player.height // 2, elapsed_time))
+
+        trail_points = [point for point in trail_points if elapsed_time - point[2] < TRAIL_LIFETIME]
 
         for star in stars[:]:
             star.y += STAR_VEL + int(elapsed_time/20)
@@ -122,7 +143,15 @@ def main():
             pygame.time.delay(4000)
             break
 
-        draw(player, elapsed_time, stars, bstars)
+        if player.y <= 0:
+            benito_mode = True
+
+        if benito_mode:
+            current_player_sprite = benito_image
+        else:
+            current_player_sprite = tommy_image
+
+        draw(player, current_player_sprite, elapsed_time, stars, bstars, trail_points)
 
     pygame.quit()
 
